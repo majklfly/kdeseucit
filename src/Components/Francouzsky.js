f04ab829c7b813da65410386ba5e507bea53027b
majklfly <majklfly@gmail.com>
2021-01-13 19:29:48 +0000

    jazyky finished

--- src/Components/Francouzsky.js
+++ src/Components/Francouzsky.js
@@ -1,12 +1,28 @@
 import React from 'react';
 
 import snadhledem from "../Pics/snadhledem.jpg";
+import ctedu from "../Pics/ctedu.jpg";
+import tvmonde from "../Pics/tvmonde.jpg";
+import clic from "../Pics/clic.jpg";
+import hugo from "../Pics/hugo.jpg";
 
 export const Francouzsky = () => {
     return (
         <div className="BoxProKarty">
             <a href="https://www.snadhledem.cz/" target="_blank" rel="noreferrer">
                 <img className="obrazekKarty" src={snadhledem} alt="snadhledem" />
+            </a>
+            <a href="https://edu.ceskatelevize.cz/predmet/francouzsky-jazyk" target="_blank" rel="noreferrer">
+                <img className="obrazekKarty" src={ctedu} alt="ctedu" />
+            </a>
+            <a href="https://edu.ceskatelevize.cz/predmet/francouzsky-jazyk" target="_blank" rel="noreferrer">
+                <img className="obrazekKarty" src={tvmonde} alt="tvmonde" />
+            </a>
+            <a href="http://www.cndp.fr/crdp-dijon/-Clic-images-.html" target="_blank" rel="noreferrer">
+                <img className="obrazekKarty" src={clic} alt="clic" />
+            </a>
+            <a href="https://www.hugolescargot.com/" target="_blank" rel="noreferrer">
+                <img className="obrazekKarty" src={hugo} alt="hugo" />
             </a>
         </div>
     )
--- src/Components/Nemecky.js
+++ src/Components/Nemecky.js
@@ -1,12 +1,32 @@
 import React from 'react';
 
 import umimenemecky from "../Pics/umimenemecky.jpg";
+import snadhledem from "../Pics/snadhledem.jpg";
+import ctedu from "../Pics/ctedu.jpg";
+import testpark from "../Pics/testpark.jpg";
+import veseledeti2 from "../Pics/veseledeti2.jpg";
+import freunde from "../Pics/freunde.jpg";
 
 export const Nemecky = () => {
     return (
         <div className="BoxProKarty">
             <a href="https://www.umimenemecky.cz/" target="_blank" rel="noreferrer">
                 <img className="obrazekKarty" src={umimenemecky} alt="umimenemecky" />
+            </a>
+            <a href="https://www.skolasnadhledem.cz/profil/cizi-jazyky/1072-nemecky-jazyk" target="_blank" rel="noreferrer">
+                <img className="obrazekKarty" src={snadhledem} alt="snadhledem" />
+            </a>
+            <a href="https://edu.ceskatelevize.cz/predmet/nemecky-jazyk?stupen=1-stupen-zs" target="_blank" rel="noreferrer">
+                <img className="obrazekKarty" src={ctedu} alt="ctedu" />
+            </a>
+            <a href="http://www.testpark.cz/testy/nemcina" target="_blank" rel="noreferrer">
+                <img className="obrazekKarty" src={testpark} alt="testpark" />
+            </a>
+            <a href="https://www.ucenionline.com/jazyky/jazyky/nemecky-jazyk/" target="_blank" rel="noreferrer">
+                <img className="obrazekKarty" src={veseledeti2} alt="veseledeti2" />
+            </a>
+            <a href="https://www.hueber.de/cz/beste-freunde/online/uebungen" target="_blank" rel="noreferrer">
+                <img className="obrazekKarty" src={freunde} alt="freunde" />
             </a>
         </div>
     )
