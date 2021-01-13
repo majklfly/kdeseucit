ecf0dc90f68b13f48a1cc1e197c0ddd2d90b92d6
majklfly <majklfly@gmail.com>
2021-01-13 16:22:32 +0000

    added vytvarny

--- src/Components/Hudebni.js
+++ src/Components/Hudebni.js
@@ -1,12 +1,20 @@
 import React from 'react';
 
 import ddm from "../Pics/ddm.jpg";
+import kytara from "../Pics/kytara.jpg";
+import ctedu from "../Pics/ctedu.jpg";
 
 export const Hudebni = () => {
     return (
         <div className="BoxProKarty">
             <a href="https://mitkamjit.cz/?zamereni=1&vek=" target="_blank" rel="noreferrer">
                 <img className="obrazekKarty" src={ddm} alt="ddm" />
+            </a>
+            <a href="http://www.kytara.cz/" target="_blank" rel="noreferrer">
+                <img className="obrazekKarty" src={kytara} alt="kytara" />
+            </a>
+            <a href="https://edu.ceskatelevize.cz/predmet/hudebni-a-pohybove-aktivity" target="_blank" rel="noreferrer">
+                <img className="obrazekKarty" src={ctedu} alt="ctedu" />
             </a>
         </div>
     )
--- src/Components/Vytvarne.js
+++ src/Components/Vytvarne.js
@@ -1,12 +1,48 @@
 import React from 'react';
 
 import ddm from "../Pics/ddm.jpg";
+import ctedu from "../Pics/ctedu.jpg";
+import krokotak from "../Pics/krokotak.jpg";
+import juch from "../Pics/juch.jpg";
+import napady from "../Pics/napady.jpg";
+import hello from "../Pics/hello.jpg";
+import lizard from "../Pics/lizard.jpg";
+import pinterest from "../Pics/pinterest.jpg";
+import grafeditor from "../Pics/grafeditor.jpg";
+import village from "../Pics/village.jpg";
 
 export const Vytvarne = () => {
     return (
         <div className="BoxProKarty">
             <a href="https://mitkamjit.cz/?zamereni=1&vek=" target="_blank" rel="noreferrer">
                 <img className="obrazekKarty" src={ddm} alt="ddm" />
+            </a>
+            <a href="https://edu.ceskatelevize.cz/predmet/vytvarne-aktivity" target="_blank" rel="noreferrer">
+                <img className="obrazekKarty" src={ctedu} alt="ctedu" />
+            </a>
+            <a href="http://krokotak.com/" target="_blank" rel="noreferrer">
+                <img className="obrazekKarty" src={krokotak} alt="krokotak" />
+            </a>
+            <a href="http://www.juchuuu.cz/stahovani/" target="_blank" rel="noreferrer">
+                <img className="obrazekKarty" src={juch} alt="juch" />
+            </a>
+            <a href="https://www.napadyproanicku.cz/" target="_blank" rel="noreferrer">
+                <img className="obrazekKarty" src={napady} alt="napady" />
+            </a>
+            <a href="https://www.hellowonderful.co/category/create/" target="_blank" rel="noreferrer">
+                <img className="obrazekKarty" src={hello} alt="hello" />
+            </a>
+            <a href="https://lizardpoint.com/art/index.php" target="_blank" rel="noreferrer">
+                <img className="obrazekKarty" src={lizard} alt="lizard" />
+            </a>
+            <a href="https://cz.pinterest.com/" target="_blank" rel="noreferrer">
+                <img className="obrazekKarty" src={pinterest} alt="pinterest" />
+            </a>
+            <a href="https://www.canva.com/cs_cz/" target="_blank" rel="noreferrer">
+                <img className="obrazekKarty" src={grafeditor} alt="grafeditor" />
+            </a>
+            <a href="https://www.activityvillage.co.uk/" target="_blank" rel="noreferrer">
+                <img className="obrazekKarty" src={village} alt="village" />
             </a>
         </div>
     )
