48fe953476d61e8a223824c10a27829503518ab9
majklfly <majklfly@gmail.com>
2021-01-13 19:09:38 +0000

    added rozsirujici

--- src/Components/Rozsirujici.js
+++ src/Components/Rozsirujici.js
@@ -1,12 +1,148 @@
 import React from 'react';
 
 import lustenky from "../Pics/lustenky.jpg";
+import ddm from "../Pics/ddm.jpg";
+import scio from "../Pics/scio.jpg";
+import zlatka from "../Pics/zlatka.jpg";
+import kriticke from "../Pics/kriticke.jpg";
+import vedanadoma from "../Pics/vedanadoma.jpg";
+import tydenvedy from "../Pics/tydenvedy.jpg";
+import chapes from "../Pics/chapes.jpg";
+import soboty from "../Pics/soboty.jpg";
+import ctedu from "../Pics/ctedu.jpg";
+import odpoledka from "../Pics/odpoledka.jpg";
+import rozhlasctenar from "../Pics/rozhlasctenar.jpg";
+import rozhlasprodeti from "../Pics/rozhlasprodeti.jpg";
+import rozhlastemata from "../Pics/rozhlastemata.jpg";
+import nenudim from "../Pics/nenudim.jpg";
+import cez from "../Pics/cez.jpg";
+import mo from "../Pics/mo.jpg";
+import fo from "../Pics/fo.jpg";
+import klokan from "../Pics/klokan.jpg";
+import turnaj from "../Pics/turnaj.jpg";
+import gramotnost from "../Pics/gramotnost.jpg";
+import badatelna from "../Pics/badatelna.jpg";
+import kladivo from "../Pics/kladivo.jpg";
+import projektovevyucovani from "../Pics/projektovevyucovani.jpg";
+import sotkoviny from "../Pics/sotkoviny.jpg";
+import povidkar from "../Pics/povidkar.jpg";
+import vojenskelesy from "../Pics/vojenskelesy.jpg";
+import podyji from "../Pics/podyji.jpg";
+import krkonose from "../Pics/krkonose.jpg";
+import lesy from "../Pics/lesy.jpg";
+import svycarsko from "../Pics/svycarsko.jpg";
+import veronica from "../Pics/veronica.jpg";
+import quizlet from "../Pics/quizlet.jpg";
+import doprava from "../Pics/doprava.jpg";
+import cesty from "../Pics/cesty.jpg";
 
 export const Rozsirujici = () => {
     return (
         <div className="BoxProKarty">
             <a href="https://rysava.websnadno.cz/" target="_blank" rel="noreferrer">
                 <img className="obrazekKarty" src={lustenky} alt="lustenky" />
+            </a>
+            <a href="https://mitkamjit.cz/?zamereni=14&vek=" target="_blank" rel="noreferrer">
+                <img className="obrazekKarty" src={ddm} alt="ddm" />
+            </a>
+            <a href="https://www.ucimesedoma.cz/" target="_blank" rel="noreferrer">
+                <img className="obrazekKarty" src={scio} alt="scio" />
+            </a>
+            <a href="https://www.zlatka.in/cs/" target="_blank" rel="noreferrer">
+                <img className="obrazekKarty" src={zlatka} alt="zlatka" />
+            </a>
+            <a href="https://www.trainbra.in/cs/" target="_blank" rel="noreferrer">
+                <img className="obrazekKarty" src={kriticke} alt="kriticke" />
+            </a>
+            <a href="https://www.avcr.cz/cs/pro-verejnost/veda-na-doma/" target="_blank" rel="noreferrer">
+                <img className="obrazekKarty" src={vedanadoma} alt="vedanadoma" />
+            </a>
+            <a href="https://www.tydenvedy.cz/festival/best-of-tvt/" target="_blank" rel="noreferrer">
+                <img className="obrazekKarty" src={tydenvedy} alt="tydenvedy" />
+            </a>
+            <a href="https://chapes.cz/" target="_blank" rel="noreferrer">
+                <img className="obrazekKarty" src={chapes} alt="chapes" />
+            </a>
+            <a href="https://www.ctm-academy.cz/" target="_blank" rel="noreferrer">
+                <img className="obrazekKarty" src={soboty} alt="soboty" />
+            </a>
+            <a href="https://edu.ceskatelevize.cz/" target="_blank" rel="noreferrer">
+                <img className="obrazekKarty" src={ctedu} alt="ctedu" />
+            </a>
+            <a href="https://www.ceskatelevize.cz/tv-program/hledani/?filtr%5bnadtitul%5d=Odpoledka" target="_blank" rel="noreferrer">
+                <img className="obrazekKarty" src={odpoledka} alt="odpoledka" />
+            </a>
+            <a href="https://temata.rozhlas.cz/ctenarskydenik" target="_blank" rel="noreferrer">
+                <img className="obrazekKarty" src={rozhlasctenar} alt="rozhlasctenar" />
+            </a>
+            <a href="https://www.mujrozhlas.cz/pro-deti" target="_blank" rel="noreferrer">
+                <img className="obrazekKarty" src={rozhlasprodeti} alt="rozhlasprodeti" />
+            </a>
+            <a href="https://temata.rozhlas.cz/" target="_blank" rel="noreferrer">
+                <img className="obrazekKarty" src={rozhlastemata} alt="rozhlastemata" />
+            </a>
+            <a href="https://decko.ceskatelevize.cz/nenudim-se-doma/vzdelavani" target="_blank" rel="noreferrer">
+                <img className="obrazekKarty" src={nenudim} alt="nenudim" />
+            </a>
+            <a href="https://www.svetenergie.cz/" target="_blank" rel="noreferrer">
+                <img className="obrazekKarty" src={cez} alt="cez" />
+            </a>
+            <a href="http://www.matematickaolympiada.cz/" target="_blank" rel="noreferrer">
+                <img className="obrazekKarty" src={mo} alt="mo" />
+            </a>
+            <a href="http://fyzikalniolympiada.cz/" target="_blank" rel="noreferrer">
+                <img className="obrazekKarty" src={fo} alt="fo" />
+            </a>
+            <a href="http://matematickyklokan.net/" target="_blank" rel="noreferrer">
+                <img className="obrazekKarty" src={klokan} alt="klokan" />
+            </a>
+            <a href="https://tmf.fzu.cz/tasks.php?y" target="_blank" rel="noreferrer">
+                <img className="obrazekKarty" src={turnaj} alt="turnaj" />
+            </a>
+            <a href="http://www.ctenarska-gramotnost.cz/" target="_blank" rel="noreferrer">
+                <img className="obrazekKarty" src={gramotnost} alt="gramotnost" />
+            </a>
+            <a href="https://www.youtube.com/user/ScienceMuni/videos" target="_blank" rel="noreferrer">
+                <img className="obrazekKarty" src={badatelna} alt="badatelna" />
+            </a>
+            <a href="https://www.youtube.com/user/VedeckeKladivo/videos" target="_blank" rel="noreferrer">
+                <img className="obrazekKarty" src={kladivo} alt="kladivo" />
+            </a>
+            <a href="http://www.projektovavyuka.cz/" target="_blank" rel="noreferrer">
+                <img className="obrazekKarty" src={projektovevyucovani} alt="projektovevyucovani" />
+            </a>
+            <a href="http://www.sotkoviny.cz/" target="_blank" rel="noreferrer">
+                <img className="obrazekKarty" src={sotkoviny} alt="sotkoviny" />
+            </a>
+            <a href="https://www.renenekuda.cz/povidkar/" target="_blank" rel="noreferrer">
+                <img className="obrazekKarty" src={povidkar} alt="povidkar" />
+            </a>
+            <a href="https://www.npsumava.cz/navstivte-sumavu/materialy-ke-stazeni/samoobsluzne-pracovni-listy/" target="_blank" rel="noreferrer">
+                <img className="obrazekKarty" src={vojenskelesy} alt="vojenskelesy" />
+            </a>
+            <a href="https://www.nppodyji.cz/sdetmidoma" target="_blank" rel="noreferrer">
+                <img className="obrazekKarty" src={podyji} alt="podyji" />
+            </a>
+            <a href="https://lesycr.cz/navstevnici-lesa/" target="_blank" rel="noreferrer">
+                <img className="obrazekKarty" src={krkonose} alt="krkonose" />
+            </a>
+            <a href="https://www.krnap.cz/brozury/" target="_blank" rel="noreferrer">
+                <img className="obrazekKarty" src={lesy} alt="lesy" />
+            </a>
+            <a href="https://www.npcs.cz/evvopublikace" target="_blank" rel="noreferrer">
+                <img className="obrazekKarty" src={svycarsko} alt="svycarsko" />
+            </a>
+            <a href="http://www.casopisveronica.cz/" target="_blank" rel="noreferrer">
+                <img className="obrazekKarty" src={veronica} alt="veronica" />
+            </a>
+            <a href="https://quizlet.com/" target="_blank" rel="noreferrer">
+                <img className="obrazekKarty" src={quizlet} alt="quizlet" />
+            </a>
+            <a href="https://www.dopravnivychova.cz/" target="_blank" rel="noreferrer">
+                <img className="obrazekKarty" src={doprava} alt="doprava" />
+            </a>
+            <a href="https://www.bezpecnecesty.cz/cz/dopravni-vychova" target="_blank" rel="noreferrer">
+                <img className="obrazekKarty" src={cesty} alt="cesty" />
             </a>
         </div>
     )
