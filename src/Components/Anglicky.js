d4ba332330988f99bb1b006758deeadf730ef8bf
VeronikaKovarova <veronika.kovarova@pirati.cz>
2021-01-13 20:11:32 +0100

    komentar

--- src/Components/Anglicky.js
+++ src/Components/Anglicky.js
@@ -1,12 +1,124 @@
 import React from 'react';
 
 import skolakov from "../Pics/skolakov.jpg";
+import cvicenitesty from "../Pics/cvicenitesty.jpg";
+import happyhouse from "../Pics/happyhouse.jpg";
+import playgroundenglish from "../Pics/playgroundenglish.jpg";
+import umimeanglicky from "../Pics/umimeanglicky.jpg";
+import snadhledem from "../Pics/snadhledem.jpg";
+import ctedu from "../Pics/ctedu.jpg";
+import walrus from "../Pics/walrus.jpg";
+import pronejmensi from "../Pics/pronejmensi.jpg";
+import monkeyenglish from "../Pics/monkeyenglish.jpg";
+import software from "../Pics/software.jpg";
+import chmelaj from "../Pics/chmelaj.jpg";
+import ict from "../Pics/ict.jpg";
+import village from "../Pics/village.jpg";
+import cbeebies from "../Pics/cbeebies.jpg";
+import duolingo from "../Pics/duolingo.jpg";
+import kidspages from "../Pics/kidspages.jpg";
+import magic from "../Pics/magic.jpg";
+import singing from "../Pics/singing.jpg";
+import help from "../Pics/help.jpg";
+import veseledeti from "../Pics/veseledeti.jpg";
+import vyslovnost from "../Pics/vyslovnost.jpg";
+import bbc from "../Pics/bbc.jpg";
+import testpark from "../Pics/testpark.jpg";
+import seduo from "../Pics/seduo.jpg";
+import council from "../Pics/council.jpg";
+import brona from "../Pics/brona.jpg";
+import webster from "../Pics/webster.jpg";
+import skill from "../Pics/skill.jpg";
 
 export const Anglicky = () => {
     return (
         <div className="BoxProKarty">
             <a href="https://skolakov.eu/" target="_blank" rel="noreferrer">
                 <img className="obrazekKarty" src={skolakov} alt="skolakov" />
+            </a>
+            <a href="http://cviceni.testy.sweb.cz/anglictina.htm" target="_blank" rel="noreferrer">
+                <img className="obrazekKarty" src={cvicenitesty} alt="Cvičení testy" />
+            </a>
+            <a href="https://elt.oup.com/parents/resources/oxfordparents/?cc=cz&selLanguage=cs&mode=hub" target="_blank" rel="noreferrer">
+                <img className="obrazekKarty" src={happyhouse} alt="Happy house" />
+            </a>
+            <a href="https://www.playgroundenglish.cz/worksheets" target="_blank" rel="noreferrer">
+                <img className="obrazekKarty" src={playgroundenglish} alt="Play ground English" />
+            </a>
+            <a href="https://www.umimeanglicky.cz/" target="_blank" rel="noreferrer">
+                <img className="obrazekKarty" src={umimeanglicky} alt="Umíme anglicky" />
+            </a>
+            <a href="https://www.skolasnadhledem.cz/profil/prehled" target="_blank" rel="noreferrer">
+                <img className="obrazekKarty" src={snadhledem} alt="sUčení s nadhledem" />
+            </a>
+            <a href="https://edu.ceskatelevize.cz/predmet/anglicky-jazyk?stupen=1-stupen-zs" target="_blank" rel="noreferrer">
+                <img className="obrazekKarty" src={ctedu} alt="ČT edu" />
+            </a>
+            <a href="https://www.youtube.com/user/SingingWalrusMusic" target="_blank" rel="noreferrer">
+                <img className="obrazekKarty" src={walrus} alt="Singing walrus" />
+            </a>
+            <a href="https://www.youtube.com/watch?v=3EllY8Yq-BI&list=PLIhCv3wSpU7PjN99pPkek4ZT1qrsPzVbb" target="_blank" rel="noreferrer">
+                <img className="obrazekKarty" src={pronejmensi} alt="Angličtina pro nejmenší" />
+            </a>
+            <a href="http://monkeyenglish.cz/" target="_blank" rel="noreferrer">
+                <img className="obrazekKarty" src={monkeyenglish} alt="Monkey English" />
+            </a>
+            <a href="https://www.sheppardsoftware.com/" target="_blank" rel="noreferrer">
+                <img className="obrazekKarty" src={software} alt="Sheppard software" />
+            </a>
+            <a href="http://www.chmelkova.cz/" target="_blank" rel="noreferrer">
+                <img className="obrazekKarty" src={chmelaj} alt="Chmelková angličtina" />
+            </a>
+            <a href="https://www.ictgames.com/mobilePage/literacy.html" target="_blank" rel="noreferrer">
+                <img className="obrazekKarty" src={ict} alt="Ict games" />
+            </a>
+            <a href="https://www.activityvillage.co.uk/learning" target="_blank" rel="noreferrer">
+                <img className="obrazekKarty" src={village} alt="Activity village" />
+            </a>
+            <a href="https://global.cbeebies.com/" target="_blank" rel="noreferrer">
+                <img className="obrazekKarty" src={cbeebies} alt="Cbeebies" />
+            </a>
+            <a href="https://www.duolingo.com/learn" target="_blank" rel="noreferrer">
+                <img className="obrazekKarty" src={duolingo} alt="Duolingo" />
+            </a>
+            <a href="https://www.kids-pages.com" target="_blank" rel="noreferrer">
+                <img className="obrazekKarty" src={kidspages} alt="Kid´s pages" />
+            </a>
+            <a href="https://www.youtube.com/playlist?list=PLAHm1JQMiqaSsrD17a2YQgNuArlXmxQGt" target="_blank" rel="noreferrer">
+                <img className="obrazekKarty" src={magic} alt="Magic English" />
+            </a>
+            <a href="https://www.youtube.com/c/EnglishSingsing" target="_blank" rel="noreferrer">
+                <img className="obrazekKarty" src={singing} alt="English singing" />
+            </a>
+            <a href="https://www.helpforenglish.cz/" target="_blank" rel="noreferrer">
+                <img className="obrazekKarty" src={help} alt="Help for English" />
+            </a>
+            <a href="https://www.ucenionline.com/jazyky/jazyky/anglicky-jazyk/" target="_blank" rel="noreferrer">
+                <img className="obrazekKarty" src={veseledeti} alt="Veselé děti" />
+            </a>
+            <a href="https://www.youtube.com/channel/UCdR-hKRaYAZUn2FAHb-xMcQ" target="_blank" rel="noreferrer">
+                <img className="obrazekKarty" src={vyslovnost} alt="Výslovnost" />
+            </a>
+            <a href="https://www.bbc.co.uk/learningenglish/english/" target="_blank" rel="noreferrer">
+                <img className="obrazekKarty" src={bbc} alt="bbc" />
+            </a>
+            <a href="http://www.testpark.cz/testy/anglictina" target="_blank" rel="noreferrer">
+                <img className="obrazekKarty" src={testpark} alt="Test park" />
+            </a>
+            <a href="https://www.seduo.cz/naucte-se-konecne-opravdu-anglicky" target="_blank" rel="noreferrer">
+                <img className="obrazekKarty" src={seduo} alt="seduo" />
+            </a>
+            <a href="http://learnenglishkids.britishcouncil.org/" target="_blank" rel="noreferrer">
+                <img className="obrazekKarty" src={council} alt="British council" />
+            </a>
+            <a href="https://www.youtube.com/user/BBSobotka" target="_blank" rel="noreferrer">
+                <img className="obrazekKarty" src={brona} alt="Broňa Sobotka" />
+            </a>
+            <a href="https://learnersdictionary.com/" target="_blank" rel="noreferrer">
+                <img className="obrazekKarty" src={webster} alt="Webster" />
+            </a>
+            <a href="https://www.skillsbuilder.org/homelearning" target="_blank" rel="noreferrer">
+                <img className="obrazekKarty" src={skill} alt="Skills builder" />
             </a>
         </div>
     )
