467a38d1cdb727102e7e5e1d6e1fa5ad4c415e1d
VeronikaKovarova <veronika.kovarova@pirati.cz>
2021-01-14 08:23:48 +0100

    vlastiveda prirodopis

--- src/Components/Prirodopis.js
+++ src/Components/Prirodopis.js
@@ -1,6 +1,27 @@
 import React from 'react';
 
 import snadhledem from "../Pics/snadhledem.jpg";
+import umimefakta from "../Pics/umimefakta.jpg";
+import curko from "../Pics/curko.jpg";
+import khan from "../Pics/khan.jpg";
+import nabla from "../Pics/nabla.jpg";
+import ctedu from "../Pics/ctedu.jpg";
+import odpoledka from "../Pics/odpoledka.jpg";
+import ptaciporadna from "../Pics/ptaciporadna.jpg";
+import testpark from "../Pics/testpark.jpg";
+import rozhlastemata from "../Pics/rozhlastemata.jpg";
+import vedanadoma from "../Pics/vedanadoma.jpg";
+import zadnaveda from "../Pics/zadnaveda.jpg";
+import kladivo from "../Pics/kladivo.jpg";
+import ddm from "../Pics/ddm.jpg";
+import veseledeti from "../Pics/veseledeti.jpg";
+import veronica from "../Pics/veronica.jpg";
+import sumava from "../Pics/sumava.jpg";
+import svycarsko from "../Pics/svycarsko.jpg";
+import podyji from "../Pics/podyji.jpg";
+import krkonose from "../Pics/krkonose.jpg";
+import vojenskelesy from "../Pics/vojenskelesy.jpg";
+import lesy from "../Pics/lesy.jpg";
 
 export const Prirodopis = () => {
     return (
@@ -8,6 +29,68 @@ export const Prirodopis = () => {
             <a href="https://www.skolasnadhledem.cz/profil/2-stupen/881-prirodopis" target="_blank" rel="noreferrer">
                 <img className="obrazekKarty" src={snadhledem} alt="snadhledem" />
             </a>
+            <a href="https://www.umimefakta.cz/biologie" target="_blank" rel="noreferrer">
+                <img className="obrazekKarty" src={umimefakta} alt="Umíme fakta" />
+            </a>
+            <a href="https://www.youtube.com/user/anubiskt/videos" target="_blank" rel="noreferrer">
+                <img className="obrazekKarty" src={curko} alt="Petr Curko" />
+            </a>
+            <a href="https://khanovaskola.cz/" target="_blank" rel="noreferrer">
+                <img className="obrazekKarty" src={khan} alt="Khanova škola" />
+            </a>
+            <a href="http://www.nabla.cz/obsah/biologie/" target="_blank" rel="noreferrer">
+                <img className="obrazekKarty" src={nabla} alt="nabla" />
+            </a>
+            <a href="https://edu.ceskatelevize.cz/predmet/prirodopis" target="_blank" rel="noreferrer">
+                <img className="obrazekKarty" src={ctedu} alt="ČTedu" />
+            </a>
+            <a href="https://www.ceskatelevize.cz/tv-program/hledani/?filtr%5bnadtitul%5d=Odpoledka" target="_blank" rel="noreferrer">
+                <img className="obrazekKarty" src={odpoledka} alt="Odpoledka" />
+            </a>
+            <a href="https://www.facebook.com/groups/594820014006582/" target="_blank" rel="noreferrer">
+                <img className="obrazekKarty" src={ptaciporadna} alt="Ptačí poradna" />
+            </a>
+            <a href="http://www.testpark.cz/testy/biologie" target="_blank" rel="noreferrer">
+                <img className="obrazekKarty" src={testpark} alt="Test park" />
+            </a>
+            <a href="https://temata.rozhlas.cz/" target="_blank" rel="noreferrer">
+                <img className="obrazekKarty" src={rozhlastemata} alt="Rozhlas témata" />
+            </a>
+            <a href="https://www.avcr.cz/cs/pro-verejnost/veda-na-doma/" target="_blank" rel="noreferrer">
+                <img className="obrazekKarty" src={vedanadoma} alt="Věda na doma" />
+            </a>
+            <a href="http://www.vedaneniveda.cz/vyukove-materialy-pro/2-stupen-zakladnich-skol/prirodopis" target="_blank" rel="noreferrer">
+                <img className="obrazekKarty" src={zadnaveda} alt="Věda není žádná věda" />
+            </a>
+            <a href="https://www.youtube.com/user/VedeckeKladivo/videos" target="_blank" rel="noreferrer">
+                <img className="obrazekKarty" src={kladivo} alt="Vědecké kladivo" />
+            </a>
+            <a href="https://mitkamjit.cz/?zamereni=3&vek=" target="_blank" rel="noreferrer">
+                <img className="obrazekKarty" src={ddm} alt="Dům dětí a mládeže" />
+            </a>
+            <a href="https://www.ucenionline.com/priroda/" target="_blank" rel="noreferrer">
+                <img className="obrazekKarty" src={veseledeti} alt="veselé děti" />
+            </a>
+            <a href="http://www.casopisveronica.cz/" target="_blank" rel="noreferrer">
+                <img className="obrazekKarty" src={veronica} alt="veronica" />
+            </a>
+            <a href="https://www.npsumava.cz/navstivte-sumavu/materialy-ke-stazeni/samoobsluzne-pracovni-listy/" target="_blank" rel="noreferrer">
+                <img className="obrazekKarty" src={sumava} alt="Národní park Šumava" />
+            </a>
+            <a href="https://www.nppodyji.cz/sdetmidoma" target="_blank" rel="noreferrer">
+                <img className="obrazekKarty" src={podyji} alt="Národní park Podyjí" />
+            </a>
+            <a href="https://www.krnap.cz/brozury/" target="_blank" rel="noreferrer">
+                <img className="obrazekKarty" src={krkonose} alt="Krkonošský národní park" />
+            </a>
+            <a href="https://deti.vls.cz/" target="_blank" rel="noreferrer">
+                <img className="obrazekKarty" src={vojenskelesy} alt="Vojenské lesy" />
+            </a>
+            <a href="https://lesycr.cz/navstevnici-lesa/" target="_blank" rel="noreferrer">
+                <img className="obrazekKarty" src={lesy} alt="Lesy České republiky" />
+            </a>
+    
+
         </div>
     )
 }
--- src/Components/Vlastiveda.js
+++ src/Components/Vlastiveda.js
@@ -1,12 +1,28 @@
 import React from 'react';
 
 import skolakov from "../Pics/skolakov.jpg";
+import lustenky from "../Pics/lustenky.jpg";
+import georgina from "../Pics/georgina.jpg";
+import snadhledem from "../Pics/snadhledem.jpg";
+import ctedu from "../Pics/ctedu.jpg";
 
 export const Vlastiveda = () => {
     return (
         <div className="BoxProKarty">
             <a href="https://skolakov.eu/" target="_blank" rel="noreferrer">
                 <img className="obrazekKarty" src={skolakov} alt="skolakov" />
+            </a>
+            <a href="https://rysava.websnadno.cz/" target="_blank" rel="noreferrer">
+                <img className="obrazekKarty" src={lustenky} alt="Luštěnky" />
+            </a>
+            <a href="https://www.geograf.in/cs/" target="_blank" rel="noreferrer">
+                <img className="obrazekKarty" src={georgina} alt="Georgina" />
+            </a>
+            <a href="https://www.skolasnadhledem.cz/profil/1-stupen/1587-spolecnost-clovek-a-jeho-svet" target="_blank" rel="noreferrer">
+                <img className="obrazekKarty" src={snadhledem} alt="Škola s nadhledem" />
+            </a>
+            <a href="https://edu.ceskatelevize.cz/predmet/prirodoveda-vlastiveda" target="_blank" rel="noreferrer">
+                <img className="obrazekKarty" src={ctedu} alt="ČT edu" />
             </a>
         </div>
     )
