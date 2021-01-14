f5743a3f78cdfa7b2c1fda1a62c88cdd6876ad59
VeronikaKovarova <veronika.kovarova@pirati.cz>
2021-01-14 10:27:15 +0100

    obcanka pracovka rodinka

--- src/Components/Rodinka.js
+++ src/Components/Rodinka.js
@@ -1,12 +1,16 @@
 import React from 'react';
 
 import hello from "../Pics/hello.jpg";
+import pinterest from "../Pics/pinterest.jpg";
 
 export const Rodinka = () => {
     return (
         <div className="BoxProKarty">
             <a href="https://www.hellowonderful.co/category/eat/" target="_blank" rel="noreferrer">
-                <img className="obrazekKarty" src={hello} alt="hello" />
+                <img className="obrazekKarty" src={hello} alt="Hello wonderful" />
+            </a>
+            <a href="https://cz.pinterest.com/" target="_blank" rel="noreferrer">
+                <img className="obrazekKarty" src={pinterest} alt="pinterest" />
             </a>
         </div>
     )
