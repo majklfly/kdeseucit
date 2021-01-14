07a030f6d8f3bdbd22f5004c8ee3ef507b250468
VeronikaKovarova <veronika.kovarova@pirati.cz>
2021-01-14 10:44:03 +0100

    ucitele

--- src/Components/Ucitele.js
+++ src/Components/Ucitele.js
@@ -1,12 +1,16 @@
 import React from 'react';
 
-import onlineknihovny from "../Pics/onlineknihovny.jpg";
+import otevrene from "../Pics/otevrene.jpg";
+import zdroje from "../Pics/zdroje.jpg";
 
 export const Ucitele = () => {
     return (
         <div className="BoxProKarty">
-            <a href="https://online.knihovny.cz/" target="_blank" rel="noreferrer">
-                <img className="obrazekKarty" src={onlineknihovny} alt="onlineknihovny" />
+            <a href="https://otevrenevzdelavani.cz/otevrene-zdroje/?fbclid=IwAR3LGKcS447xHBm7huZq-hiXYajBHk9Mc5lJthjIkkEtBqLU-OcP_laDDJU" target="_blank" rel="noreferrer">
+                <img className="obrazekKarty" src={otevrene} alt="otevrene" />
+            </a>
+            <a href="https://zdroje.guruveskole.cz/" target="_blank" rel="noreferrer">
+                <img className="obrazekKarty" src={zdroje} alt="zdroje" />
             </a>
         </div>
     )
