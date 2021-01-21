8a23b07e0662c41a1b5c24b7e58277f2fa94f690
VeronikaKovarova <veronika.kovarova@pirati.cz>
2021-01-21 23:34:22 +0100

    angl

--- src/Components/Ucitele.js
+++ src/Components/Ucitele.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 import otevrene from "../Pics/otevrene.jpg";
 import zdroje from "../Pics/zdroje.jpg";
@@ -11,35 +11,67 @@ import elixir from "../Pics/elixir.jpg";
 import zcucelozivot from "../Pics/zcucelozivot.jpg";
 
 export const Ucitele = () => {
-    return (
-        <div className="BoxProKarty">
-            <a href="https://otevrenevzdelavani.cz/otevrene-zdroje/?fbclid=IwAR3LGKcS447xHBm7huZq-hiXYajBHk9Mc5lJthjIkkEtBqLU-OcP_laDDJU" target="_blank" rel="noreferrer">
-                <img className="obrazekKarty" src={otevrene} alt="otevrene" />
-            </a>
-            <a href="https://zdroje.guruveskole.cz/" target="_blank" rel="noreferrer">
-                <img className="obrazekKarty" src={zdroje} alt="zdroje" />
-            </a>
-            <a href="https://cze-cs.techambition.com/" target="_blank" rel="noreferrer">
-                <img className="obrazekKarty" src={techambition} alt="techambition" />
-            </a>
-            <a href="https://www.ucitelnice.cz/" target="_blank" rel="noreferrer">
-                <img className="obrazekKarty" src={ucitelnice} alt="učitelnice" />
-            </a>
-            <a href="https://education.microsoft.com/cs-cz/resource/1a664892" target="_blank" rel="noreferrer">
-                <img className="obrazekKarty" src={microsoft} alt="microsoft" />
-            </a>
-            <a href="https://www.clovekvtisni.cz/co-delame/vzdelavaci-program-varianty?fbclid=IwAR1npHlcg_tdpsKSOOGmCeCsqLkQgcWUaxp4it2s_HAIH9n46si5-6ZhsEQ" target="_blank" rel="noreferrer">
-                <img className="obrazekKarty" src={clovekvtisni} alt="Člověk v tísni" />
-            </a>
-            <a href="https://whooshback.com/" target="_blank" rel="noreferrer">
-                <img className="obrazekKarty" src={legiokarty} alt="Historie v kartách" />
-            </a>
-            <a href="https://www.elixirdoskol.cz/o-elixiru/#" target="_blank" rel="noreferrer">
-                <img className="obrazekKarty" src={elixir} alt="elixir do škol" />
-            </a>
-            <a        href="https://www.youtube.com/channel/UCQEeFXqM8ugs_7S-fNjxZfQ/videos"         target="_blank"         rel="noreferrer"      >
-        <img className="obrazekKarty" src={zcucelozivot} alt="Západočeská univerzita - celoživotní vzdělávání" />
+  return (
+    <div className="BoxProKarty">
+      <a
+        href="https://otevrenevzdelavani.cz/otevrene-zdroje/?fbclid=IwAR3LGKcS447xHBm7huZq-hiXYajBHk9Mc5lJthjIkkEtBqLU-OcP_laDDJU"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img className="obrazekKarty" src={otevrene} alt="otevrene" />
       </a>
-        </div>
-    )
-}
+      <a href="https://zdroje.guruveskole.cz/" target="_blank" rel="noreferrer">
+        <img className="obrazekKarty" src={zdroje} alt="zdroje" />
+      </a>
+      <a
+        href="https://cze-cs.techambition.com/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img className="obrazekKarty" src={techambition} alt="techambition" />
+      </a>
+      <a href="https://www.ucitelnice.cz/" target="_blank" rel="noreferrer">
+        <img className="obrazekKarty" src={ucitelnice} alt="učitelnice" />
+      </a>
+      <a
+        href="https://education.microsoft.com/cs-cz/resource/1a664892"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img className="obrazekKarty" src={microsoft} alt="microsoft" />
+      </a>
+      <a
+        href="https://www.clovekvtisni.cz/co-delame/vzdelavaci-program-varianty?fbclid=IwAR1npHlcg_tdpsKSOOGmCeCsqLkQgcWUaxp4it2s_HAIH9n46si5-6ZhsEQ"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img className="obrazekKarty" src={clovekvtisni} alt="Člověk v tísni" />
+      </a>
+      <a href="https://whooshback.com/" target="_blank" rel="noreferrer">
+        <img
+          className="obrazekKarty"
+          src={legiokarty}
+          alt="Historie v kartách"
+        />
+      </a>
+      <a
+        href="https://www.elixirdoskol.cz/o-elixiru/#"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img className="obrazekKarty" src={elixir} alt="elixir do škol" />
+      </a>
+      <a
+        href="https://www.youtube.com/channel/UCQEeFXqM8ugs_7S-fNjxZfQ/videos"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img
+          className="obrazekKarty"
+          src={zcucelozivot}
+          alt="Západočeská univerzita - celoživotní vzdělávání"
+        />
+      </a>
+    </div>
+  );
+};
