e662a86b2ce48522d5e6e4bd7d85bad87061e716
VeronikaKovarova <veronika.kovarova@pirati.cz>
2021-01-23 00:54:00 +0100

    poodri

--- src/Components/Turistika.js
+++ src/Components/Turistika.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 import ctedu from "../Pics/ctedu.jpg";
 import vojenskelesy from "../Pics/vojenskelesy.jpg";
@@ -6,28 +6,52 @@ import sumava from "../Pics/sumava.jpg";
 import svycarsko from "../Pics/svycarsko.jpg";
 import podyji from "../Pics/podyji.jpg";
 import krkonose from "../Pics/krkonose.jpg";
+import poodri from "../Pics/poodri.jpg";
 
 export const Turistika = () => {
-    return (
-        <div className="BoxProKarty">
-            <a href="https://edu.ceskatelevize.cz/predmet/hudebni-a-pohybove-aktivity" target="_blank" rel="noreferrer">
-                <img className="obrazekKarty" src={ctedu} alt="ctedu" />
-            </a>
-            <a href="https://deti.vls.cz/" target="_blank" rel="noreferrer">
-                <img className="obrazekKarty" src={vojenskelesy} alt="vojenskelesy" />
-            </a>
-            <a href="https://www.npsumava.cz/navstivte-sumavu/materialy-ke-stazeni/samoobsluzne-pracovni-listy/" target="_blank" rel="noreferrer">
-                <img className="obrazekKarty" src={sumava} alt="sumava" />
-            </a>
-            <a href="https://www.npcs.cz/evvopublikace" target="_blank" rel="noreferrer">
-                <img className="obrazekKarty" src={svycarsko} alt="svycarsko" />
-            </a>
-            <a href="https://www.nppodyji.cz/sdetmidoma" target="_blank" rel="noreferrer">
-                <img className="obrazekKarty" src={podyji} alt="podyji" />
-            </a>
-            <a href="https://www.krnap.cz/brozury/" target="_blank" rel="noreferrer">
-                <img className="obrazekKarty" src={krkonose} alt="krkonose" />
-            </a>
-        </div>
-    )
-}
+  return (
+    <div className="BoxProKarty">
+      <a
+        href="https://edu.ceskatelevize.cz/predmet/hudebni-a-pohybove-aktivity"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img className="obrazekKarty" src={ctedu} alt="ctedu" />
+      </a>
+      <a href="https://deti.vls.cz/" target="_blank" rel="noreferrer">
+        <img className="obrazekKarty" src={vojenskelesy} alt="vojenskelesy" />
+      </a>
+      <a
+        href="https://www.npsumava.cz/navstivte-sumavu/materialy-ke-stazeni/samoobsluzne-pracovni-listy/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img className="obrazekKarty" src={sumava} alt="sumava" />
+      </a>
+      <a
+        href="https://www.npcs.cz/evvopublikace"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img className="obrazekKarty" src={svycarsko} alt="svycarsko" />
+      </a>
+      <a
+        href="https://www.nppodyji.cz/sdetmidoma"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img className="obrazekKarty" src={podyji} alt="podyji" />
+      </a>
+      <a href="https://www.krnap.cz/brozury/" target="_blank" rel="noreferrer">
+        <img className="obrazekKarty" src={krkonose} alt="krkonose" />
+      </a>
+      <a
+        href="https://www.poodri.com/pohadkove-poodri/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img className="obrazekKarty" src={poodri} alt="Pohádkové poodří" />
+      </a>
+    </div>
+  );
+};
